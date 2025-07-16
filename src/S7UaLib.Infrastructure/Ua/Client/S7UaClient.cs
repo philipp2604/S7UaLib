98bef14ab1c2b13db083eb0796a47241589d01f3
Philipp Siewert <philipp@phsiewert.de>
2025-07-16 07:34:15 +0200

    remove debug command
    fix integration tests

--- src/S7UaLib.Infrastructure/Ua/Client/S7UaClient.cs
+++ src/S7UaLib.Infrastructure/Ua/Client/S7UaClient.cs
@@ -360,7 +360,7 @@ internal class S7UaClient : IS7UaClient, IDisposable
         {
             throw new SystemException("Application instance certificate invalid!");
         }
-        SaveConfiguration("testconfig.json");
+
         _appInst.ApplicationConfiguration.CertificateValidator.CertificateValidation += Client_CertificateValidation;
     }
 
